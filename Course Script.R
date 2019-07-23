2fcf4c60a7f72d99964113ad04f6ca526e7d71b8
KHNajafi <kh.najafi@gmail.com>
2019-07-23 13:34:11 -0400

    Added mini UI gadget example function w/ defined numerical inputs and product command line output

--- Course Script.R
+++ Course Script.R
@@ -13,3 +13,47 @@
 libraries <- c("data.table", "tidyverse", "lubridate", "shiny", "miniUI")
 lapply(libraries, require, character.only = T)
 rm(libraries)
+
+
+
+# # # # # # # # # #
+
+
+
+#### CODE EXAMPLES ####
+
+#### Gadget - Mini UI Example ####
+require(shiny)
+require(miniUI)
+
+mini_UI_example <- function(n1, n2) {
+
+        # Still require UI and server Shiny objects
+        ui <- miniPage(
+                gadgetTitleBar("Mini & Useful on Mobile"),
+                p("This is what text looks like on a mini page Shiny App",
+                  "Here is an input gadget:"),
+                miniContentPanel(
+                        p("Within this panel we'll demonstrate inputs"),
+                        #Input arguments are defined in the parent function above
+                        selectInput("n01", "Choose your first number:", n1),
+                        selectInput("n02", "Choose your second number:", n2)
+                )
+
+        )
+
+        server <- function(input, output, session) {
+                # A button, with JS event to close upon click
+                # We can take the inputs from the UI within `observeEvent`
+                observeEvent(input$done, {
+                        num01 <- as.integer(input$n01)
+                        num02 <- as.integer(input$n02)
+
+                        # And transform within `StopApp`
+                        stopApp(num01 * num02)
+                })
+        }
+
+        # Compiling app
+        runGadget(ui, server)
+}
