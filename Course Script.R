26c1b63ad012ef3e3101011e16a5e66156b104d7
KHNajafi <kh.najafi@gmail.com>
2019-07-23 13:09:27 -0400

    Created script file for code examples and exercises

---
+++ Course Script.R
@@ -0,0 +1,15 @@
+## Course Notes
+## Developing Data Products - JHU Coursera
+## July 2019
+## Khalil H Najafi
+##
+## ABSTRACT:
+## Rough script file containing code examples and exercises
+
+# # # # # # # # # #
+
+###### FUNCTIONS & WORKSPACE ######
+# REQUIRED LIBRARIES
+libraries <- c("data.table", "tidyverse", "lubridate", "shiny", "miniUI")
+lapply(libraries, require, character.only = T)
+rm(libraries)
